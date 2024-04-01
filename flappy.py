a79f7dffdbb38f94cf627d98df29889de048be2b
pisanopaige <157334837+pisanopaige@users.noreply.github.com>
2024-04-01 19:25:09 -0400

    Newest update to flappy.py

--- flappy.py
+++ flappy.py
@@ -6,6 +6,7 @@ import serial
 import struct
 import time
 
+# Initialize game parameters class
 class GameParams:
     def __init__(self):
         self.scroll_speed = 6  # Adjust scroll speed
@@ -18,6 +19,7 @@ class GameParams:
         self.bg = pygame.image.load('img/bg.png')
         self.ground_img = pygame.image.load('img/ground.png')
 
+# Initialize game state class
 class GameState:
     def __init__(self):
         self.flying = False
@@ -26,16 +28,16 @@ class GameState:
         self.score = 0
         self.pass_pipe = False
         self.run = True
-        self.ground_scroll = 0
+        self.ground_scroll = 0 
         self.emg_triggered = False  # Flag to track if EMG data exceeds threshold
 
-
+# Initialize  pipe class
 class Pipe(pygame.sprite.Sprite):
     def __init__(self, x, y, position, pipe_gap, scroll_speed):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("img/pipe.png")
         self.rect = self.image.get_rect()
-        self.scroll_speed = scroll_speed
+        self.scroll_speed = scroll_speed # Pipe scroll speed is the same as game scroll speed
         if position == 1:
             self.image = pygame.transform.flip(self.image, False, True)
             self.rect.bottomleft = [x, y - int(pipe_gap / 2)]
@@ -47,7 +49,7 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
-
+# Intitialize button class
 class Button():
     def __init__(self, x, y, image):
         self.image = image
@@ -69,12 +71,13 @@ def read_serial_data(ser):
 
 def notify_callback(sender: int, data: int, gstate: GameState):
     # Threshold for EMG data
-    threshold = 303
+    threshold = 302
     if data >= threshold:
-        gstate.emg_triggered = True
+        gstate.emg_triggered = True # If Arduino data is greater than or equal to 302, EMG is triggered
     else:
         gstate.emg_triggered = False
 
+# Initialize bird class
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y, game_state):
         pygame.sprite.Sprite.__init__(self)
@@ -93,8 +96,8 @@ class Bird(pygame.sprite.Sprite):
 
     def update(self):
         if self.game_state.flying:
-            self.vel += 0.7  # Adjust falling speed
-            if self.vel > 12:  # Adjust the max falling speed
+            self.vel += 0.7  # Adjust falling speed of bird
+            if self.vel > 12:  # Adjust the max falling speed of bird
                 self.vel = 12
             if self.rect.bottom < 768:
                 self.rect.y += int(self.vel)
@@ -110,6 +113,7 @@ class Bird(pygame.sprite.Sprite):
         else:
             self.image = pygame.transform.rotate(self.images[self.index], -90)
 
+# Function to draw text and boxes on screen
 def draw_text(screen, text, font, x, y, color=(255, 255, 255), bg_color=None):
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
@@ -122,12 +126,14 @@ def draw_text(screen, text, font, x, y, color=(255, 255, 255), bg_color=None):
 
     screen.blit(text_surface, text_rect)
 
+# Function to reset the game
 def reset_game(pipe_group, flappy, gparams, gstate):
     pipe_group.empty()
     flappy.rect.x = 100
     flappy.rect.y = int(gparams.screen_height / 2)
     gstate.score = 0
 
+# Main function calls all other functions and classes
 async def main():
     pygame.init()
     gparams = GameParams()
@@ -145,6 +151,7 @@ async def main():
     # Open serial port for Arduino sensor
     ser = serial.Serial('COM7', 9600) 
 
+    # Execute while the game state is running
     while gstate.run:
         await asyncio.sleep(0.01)
         clock.tick(gparams.fps)
@@ -158,8 +165,8 @@ async def main():
         sensor_data = read_serial_data(ser)
         notify_callback(None, sensor_data, gstate)  # Call the notify_callback function with the EMG data and gstate
         if gstate.emg_triggered:  
-            flappy.vel = -10  # Make the bird jump
-            gstate.flying = True
+            flappy.vel = -10  # Set the bird's velocity to a negative value to make it jump
+            gstate.flying = True # Update the flying state to True to keep the bird flying
 
         if len(pipe_group) > 0:
             if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
@@ -168,7 +175,7 @@ async def main():
                 gstate.pass_pipe = True
             if gstate.pass_pipe:
                 if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.right:
-                    gstate.score += 1
+                    gstate.score += 1  # Add to score when bird passes through pipe
                     gstate.pass_pipe = False
         draw_text(screen, str(gstate.score), gparams.font, int(gparams.screen_width / 2), 20)
 
@@ -178,6 +185,7 @@ async def main():
             gstate.game_over = True
             gstate.flying = False
 
+        # Continue to randomly generate pipes when bird is flying and the game is not over
         if gstate.flying and not gstate.game_over:
             time_now = pygame.time.get_ticks()
             if time_now - gstate.last_pipe > gparams.pipe_frequency and len(pipe_group) < 8:  # Ensure only 6 pipes at most
